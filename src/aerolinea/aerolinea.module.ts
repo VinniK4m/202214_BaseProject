6bb9cededb182d958a21fa7428d52ea85037100c
vinnik4m <g.briceno@uniandes.edu.co>
2022-09-16 15:29:51 -0500

    Commit Persistencia

---
+++ src/aerolinea/aerolinea.module.ts
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {AerolineaEntity} from "./aerolinea.entity";
+import { AerolineaService } from './aerolinea.service';
+
+@Module({
+    imports: [TypeOrmModule.forFeature([AerolineaEntity])],
+    providers: [AerolineaService],
+})
+export class AerolineaModule {
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,17 +3,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {TypeOrmModule} from "@nestjs/typeorm";
+import { AeropuertoModule } from './aeropuerto/aeropuerto.module';
+import { AerolineaModule } from './aerolinea/aerolinea.module';
+import {AeropuertoEntity} from "./aeropuerto/aeropuerto.entity";
+import {AerolineaEntity} from "./aerolinea/aerolinea.entity";
 
 @Module({
-  imports: [
+  imports: [AeropuertoModule,AerolineaModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
       port: 5432,
-      username: 'pascialuser',
+      username: 'parcialusr',
       password: '123456',
-      database: 'gestionaerea',
-      entities: [],
+      database: 'gestionaereadb',
+      entities: [AeropuertoEntity, AerolineaEntity],
       dropSchema: true,
       synchronize: true,
       keepConnectionAlive: true
